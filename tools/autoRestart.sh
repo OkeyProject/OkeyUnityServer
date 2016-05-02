f3ae2b4c242c71171104e8a7801333683bd39162
jubeatWwW <l6104400@gmail.com>
2016-05-03 04:14:36 +0800

    some stupid big

--- tools/autoRestart.sh
+++ tools/autoRestart.sh
@@ -4,9 +4,6 @@ do
     id=`ps aux | grep Server.js | grep -v grep | awk '{print $2}'`
     sleep 30
     if [ -z $id ]; then 
-        :   
-    else
         node /home/Justin/OkeyUnityServer/src/Server.js &
     fi
-    #echo $id
 done
