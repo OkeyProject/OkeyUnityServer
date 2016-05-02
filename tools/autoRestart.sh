e125549a8c036bbf3ae734468ed820cf5022cf68
jubeatWwW <l6104400@gmail.com>
2016-05-03 04:08:15 +0800

    add auto restart script

---
+++ tools/autoRestart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while true;
+do
+    id=`ps aux | grep Server.js | grep -v grep | awk '{print $2}'`
+    sleep 30
+    if [ -z $id ]; then 
+        
+    else
+        node /home/Justin/OkeyUnityServer/src/Server.js &
+    fi
+    #echo $id
+done
